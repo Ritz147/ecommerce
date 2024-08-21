00b0a3fa1769152aa15d519b8d941ccef2ea4d1a
Ritz147 <131575805+Ritz147@users.noreply.github.com>
2024-08-21 15:13:39 +0530

    Add files via upload

---
+++ public/js/like.js
@@ -0,0 +1,35 @@
+async function likeProduct(productId,btn) {
+    try {
+        const res = await axios({
+            method: 'post',
+            url: `/products/${productId}/like`,
+            headers: {'X-Requested-With': 'XMLHttpRequest'}
+        });
+          
+    if(btn.classList.contains('fa-regular'))
+    {
+        btn.classList.remove('fa-regular');
+        btn.classList.add('fa-solid');
+    }
+    else{
+        btn.classList.remove('fa-solid');
+        btn.classList.add('fa-regular');
+    }
+    
+        console.log(res);
+}
+    catch (e) {
+        window.location.replace('/login');
+    }
+}
+
+
+window.document.addEventListener('click', (e) => {
+    const btn = e.target;
+    if (btn.classList.contains('product-like-btn')) {
+        const productId = btn.getAttribute('product-id');
+        likeProduct(productId, btn);
+    
+    }
+
+})
